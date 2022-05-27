1a8013f7b35c04705a9e170310845ca13a57950d
SalesforceAdityaTeam <aditya.cloudcrunch@gmail.com>
2022-05-27 18:15:39 +0530

    Update

--- app/src/main/java/com/example/bakaiti/MainActivity.java
+++ app/src/main/java/com/example/bakaiti/MainActivity.java
@@ -18,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
     ActivityMainBinding binding;
     FirebaseAuth auth;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
